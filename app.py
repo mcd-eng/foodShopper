29655b746a9942b38476dfc863ecc558a5e78617
mcd-eng <m.c.davies@outlook.com>
2021-11-16 22:02:27 +0000

    create database

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import os
+from azure.cosmos import CosmosClient
 
-print("Hello world")
+URL ="https://foodie.documents.azure.com:443/"
+KEY ="nZOBrQ5E6HYWVmVj1kJZju574JiWtezEET8VpgZQOIQP1AUO80IswbG2c2EWFBlPvJUUfYsEceRo7QirmCc1rw=="
+client = CosmosClient(URL, credential=KEY)
+
+database_name = 'testDatabase'
+try:
+    database = client.create_database(database_name)
+except exceptions.CosmosResourceExistsError:
+    database = client.get_database_client(database_name)
